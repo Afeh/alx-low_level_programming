3632dd9dcbc8a68e4035a005b1bdbc80dae5f78e
Afeh <balogunafebu22@gmail.com>
2023-02-21 01:02:04 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints "is it positive or negative"
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		printf("%d", i);
+		printf("\n");
+	}
+
+	return (0);
+}
