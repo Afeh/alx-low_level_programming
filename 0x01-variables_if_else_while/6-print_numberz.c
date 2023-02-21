6f50991ca160a529d36a8a55e4d8926db41dac08
Afeh <balogunafebu22@gmail.com>
2023-02-21 01:14:39 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints "is it positive or negative"
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
