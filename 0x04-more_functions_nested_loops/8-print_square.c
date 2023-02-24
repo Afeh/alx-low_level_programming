f3f68044f01caf4134499ba02dc0ced72c24791f
Afeh <balogunafebu22@gmail.com>
2023-02-24 12:21:07 +0100

    print_square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,12 +12,19 @@ void print_square(int size)
 	int i;
 	int k;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (k = 0; k < size; k++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (k = 0; k < size; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
