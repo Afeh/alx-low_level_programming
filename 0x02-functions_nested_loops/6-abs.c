d3fcec99674cea282e6d64f2911c48155508c45f
Afeh <balogunafebu22@gmail.com>
2023-02-26 22:42:56 +0100

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs- checks if a letter is in lowercase
+ * @n: variable to be checked
+ * Return: 0 or 1
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+		return (0);
+}
