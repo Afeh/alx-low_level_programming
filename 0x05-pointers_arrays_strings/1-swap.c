c3ccb6c9621af02ff0e9ab8dc11edea862dc17ae
Afeh <balogunafebu22@gmail.com>
2023-02-27 22:05:01 +0100

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - updates the value of the variable
+ * the pointer points to to 98
+ * @a: pointer to the variable to be exchanged
+ * @b: pointer to be swaped
+ */
+
+void swap_int(int *a, int *b)
+{
+	int p;
+	int l;
+
+	*a = p;
+	*b = l;
+
+	l = *a;
+	p = *b;
+}
