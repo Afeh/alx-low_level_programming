87cb44029c5aee6366636184770b85b97ca9dd78
Afeh <balogunafebu22@gmail.com>
2023-03-11 20:30:48 +0100

    Multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - is a function that prints the name.
+ *@argc: is an argument counter for char.
+ *@argv: is an argument value for char.
+ *Return: Returns always success.
+**/
+
+int main(int argc, char **argv)
+{
+	int i;
+	int result;
+
+	if (argc < 2)
+	{
+		printf("Error");
+	}
+	else
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d", result);
+	}
+
+	return (1);
+}
