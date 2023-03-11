5eeafc4f1c0293015651217b7cb7d6b548ace083
Afeh <balogunafebu22@gmail.com>
2023-03-11 20:39:38 +0100

    Return Corrections

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,13 +17,13 @@ int main(int argc, char **argv)
 	if (argc < 2)
 	{
 		printf("Error\n");
-		return (0);
+		return (1);
 	}
 	else
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
-		return (1);
+		return (0);
 	}
 
 
