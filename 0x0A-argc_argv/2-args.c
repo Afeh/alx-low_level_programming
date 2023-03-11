96c740d4d6cd69971d1d1d9875fe19fa2dae8a1d
Afeh <balogunafebu22@gmail.com>
2023-03-11 20:05:37 +0100

    Args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - is a function that prints the name.
+ *@argc: is an argument counter for char.
+ *@argv: is an argument value for char.
+ *Return: Returns always success.
+**/
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+
+	return (0);
+}
