5bf96a1a1a375e14e3e2c0ffe8136c4af3e7ceb7
Afeh <balogunafebu22@gmail.com>
2023-03-11 19:44:56 +0100

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - is a function that prints the name.
+ *@argc: is an argument counter for char.
+ *@argv: is an agument value for char.
+ *Return: Returns always success.
+**/
+
 int main(int argc, char **argv)
 {
 	(void)argc;
