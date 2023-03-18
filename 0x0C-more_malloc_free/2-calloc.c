92891cdf3c0b271e6d3b6e395cfa08e15f754528
Afeh <balogunafebu22@gmail.com>
2023-03-18 11:27:40 +0100

    Calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _calloc - finds the length of a string
+ *
+ *
+ * @nmemb: the length to be calculated
+ * @size: the size of the array
+ *
+ * Return: pointer to newly allocated string concatenation
+ *
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
