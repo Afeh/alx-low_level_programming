9f04b0a9c436590881529f7f67d8c19dae7eec73
Afeh <balogunafebu22@gmail.com>
2023-03-20 23:50:03 +0100

    new type dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a type called `dog`
+ * @name: pointer to the string containing dog's name
+ * @age: the age of the dog in float
+ * @owner: pointer to the string containing dog owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
