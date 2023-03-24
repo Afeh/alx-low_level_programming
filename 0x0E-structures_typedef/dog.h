203fca7dc7b4d74c78d7e762958885085acde370
Afeh <balogunafebu22@gmail.com>
2023-03-24 18:43:04 +0100

    Created a new tyep called dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,20 +6,21 @@
  * @name: pointer to the string containing dog's name
  * @age: the age of the dog in float
  * @owner: pointer to the string containing dog owner's name
+ *
+ * Description: This structure defines a new type
+ * called "dog", which contains information
+ * about a dog including its name, age, and owner.
+ * The name and owner are represented as pointers
+ * to strings, while the age is represented as
+ * a float value.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
