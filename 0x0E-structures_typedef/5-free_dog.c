47f9c75274311bb48fb70a85f6a39187165dcb1a
Afeh <balogunafebu22@gmail.com>
2023-03-25 00:14:25 +0100

    Freed up Memory Space

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,13 +3,19 @@
 
 /**
  * free_dog - function takes a pointer to a dog_t
- * 
+ *
  * @d: pointer to dog_t
- * 
- * Return: NULL 
+ *
+ * Return: NULL
  */
 
 void free_dog(dog_t *d)
 {
-	free(d);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+
+		free(d);
+	}
 }
