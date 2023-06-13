5c0abad222e93361905e10f37c1203f76be376e9
Afeh <balogunafebu22@gmail.com>
2023-06-13 22:46:22 +0100

    Function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,87 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * length - finds the length of a string
+ *
+ *
+ * @string: the length to be calculated
+ *
+ * Return: pointer to newly allocated string concatenation
+ *
+ */
+
+unsigned int length(char *string)
+{
+	unsigned int i;
+	unsigned int length;
+
+	length = 0;
+	i = 0;
+
+	if (string == NULL)
+	{
+		string = "";
+	}
+	else
+	{
+		while (string[i] != '\0')
+		{
+			length++;
+			i++;
+		}
+	}
+
+	return (length);
+}
+
+/**
+ * str_concat - concatenates 2 strings
+ *
+ *
+ * @s1: first string
+ * @s2: string to add to end of of first string
+ *
+ * Return: pointer to newly allocated string concatenation
+ *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int k, l;
+	unsigned int size_s1;
+	unsigned int size_s2;
+	char *concatd;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	size_s1 = length(s1);
+	size_s2 = length(s2);
+
+	concatd = malloc(size_s1 + size_s2 + 1);
+
+	if (concatd == NULL)
+	{
+		return (NULL);
+	}
+
+	for (k = 0; k < size_s1; k++)
+	{
+		concatd[k] = s1[k];
+	}
+
+	for (l = 0; l < size_s2; l++)
+	{
+		concatd[k++] = s2[l];
+	}
+
+	concatd[k] = '\0';
+
+	return (concatd);
+}
