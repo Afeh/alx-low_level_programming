7bf34647c6b9b5841cc334a055cd54eb1c845f6e
Afeh <balogunafebu22@gmail.com>
2023-06-21 21:44:49 +0100

    corrections to array_iterator function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
-#include "function_pointers.h"
 #include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  * array_iterator - prints an integer
@@ -8,17 +8,17 @@
  * @size: is the size of the array
  *
  * Return: Nothing.
-**/
+ **/
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t a;
-	if(array == NULL && action == NULL)
+	
+	if (array != NULL && action != NULL)
 	{
-		return;
-	}
-	for (a = 0; a < size; a++)
-	{
-		action(array[a]);
+		for (a = 0; a < size; a++)
+		{
+			action(array[a]);
+		}
 	}
 }
