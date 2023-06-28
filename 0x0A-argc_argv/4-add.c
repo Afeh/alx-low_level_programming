2e06173cc9341c0baba4da12c052dfc3cba1da3b
Afeh <balogunafebu22@gmail.com>
2023-06-28 09:21:23 +0100

    Made betyy corrections

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,14 +10,14 @@
  * Return: 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int sum, i, j;
 
 	if (argc == 1)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 
 	sum = 0;
@@ -25,9 +25,10 @@ int main (int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		char *num = argv[i];
+
 		for (j = 0; num[j] != '\0'; j++)
 		{
-			if (num[j] < '0' || num [j] > '9')
+			if (num[j] < '0' || num[j] > '9')
 			{
 				printf("Error\n");
 				return (1);
