92178eca8bf45d858719840b7d726c2a6498e32f
Afeh <balogunafebu22@gmail.com>
2023-06-30 11:23:03 +0100

    Fixed code errors

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,34 +2,48 @@
 #include <stdlib.h>
 
 /**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
+ * print_opcodes - Prints the opcodes of the main function.
+ * @n: The number of bytes to print.
+ */
+void print_opcodes(int n)
+{
+	unsigned char *main_ptr = (unsigned char *)print_opcodes;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x ", main_ptr[i]);
+	}
+
+	printf("\n");
+}
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments.
+ * @argv: Array containing the command line arguments.
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success, 1 or 2 on failure.
  */
 int main(int argc, char *argv[])
 {
+	int num_bytes;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int bytes = atoi(argv[1]);
+	num_bytes = atoi(argv[1]);
 
-	if (bytes < 0)
+	if (num_bytes < 0)
 	{
 		printf("Error\n");
 		return (2);
 	}
 
-	unsigned char *main_opcode = (unsigned char *)main;
-
-	for (int i = 0; i < bytes - 1; i++)
-		printf("%02hhx ", main_opcode[i]);
-
-	printf("%02hhx\n", main_opcode[bytes - 1]);
+	print_opcodes(num_bytes);
 
 	return (0);
 }
