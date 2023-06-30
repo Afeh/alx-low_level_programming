726e6e6ca94b6347d7742badf5788790c221dcdb
Afeh <balogunafebu22@gmail.com>
2023-06-30 12:09:59 +0100

    Code correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,48 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
 
 /**
  * multiply - multiplies two positive numbers
  * @num1: first number
  * @num2: second number
  */
+
+
+
 void multiply(char *num1, char *num2)
 {
-	int len1 = 0, len2 = 0, i, j, carry, prod;
-	int *result;
+	unsigned long mul = 0;
+	int i, j;
 
-	while (num1[len1])
-		len1++;
-	while (num2[len2])
-		len2++;
-
-	result = calloc(len1 + len2, sizeof(int));
-	if (result == NULL)
+	for (i = 0; num1[i] != '\0'; i++)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
+		if (num1[i] > '9' || num1[i] < '0')
 		{
-			prod = (num1[i] - '0') * (num2[j] - '0') + carry + result[i + j + 1];
-			carry = prod / 10;
-			result[i + j + 1] = prod % 10;
+			printf("Error\n");
+			exit(98);
 		}
-		result[i] += carry;
+
+		for (j = 0; num2[j] != '\0'; j++)
+		{
+			if (num2[j] > '9' || num2[j] < '0')
+			{
+				printf("Error\n");
+				exit(98);
+			}
+
+			mul = mul * 10 + ((num1[i] - '0') * (num2[j] - '0'));
+		}
 	}
 
-	i = 0;
-	while (result[i] == 0 && i < len1 + len2 - 1)
-		i++;
+	printf("%lu\n", mul);
 
-	for (; i < len1 + len2; i++)
-		printf("%d", result[i]);
-
-	printf("\n");
-	free(result);
+	return (0);
 }
