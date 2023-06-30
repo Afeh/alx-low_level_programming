0f78e278092d9a0b158e852ae1cb468ab993ed04
Afeh <balogunafebu22@gmail.com>
2023-06-30 11:03:19 +0100

    Advanced task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_opcodes - Prints the opcodes of the main function.
+ * @n: The number of bytes to print.
+ */
+void print_opcodes(int n)
+{
+	char *main_ptr = (char *)print_opcodes;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", main_ptr[i]);
+
+		if (i != n - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+}
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments.
+ * @argv: Array containing the command line arguments.
+ *
+ * Return: 0 on success, 1 or 2 on failure.
+ */
+int main(int argc, char *argv[])
+{
+	int num_bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	print_opcodes(num_bytes);
+
+	return (0);
+}
