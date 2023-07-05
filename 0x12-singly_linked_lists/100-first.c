3e83b97fe266d6f313aa48e0c221f4d83718bdaf
Afeh <balogunafebu22@gmail.com>
2023-07-05 09:39:14 +0100

    Code correctio

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * print_message - function prints first before main
+ * print_before - function prints first before main
  * Return: nothing
  */
 
+void print_before(void) __attribute__((constructor));
 
-void print_message()__attribute__((constructot));
-
-void print_message()
+void print_before(void)
 {
-	printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
