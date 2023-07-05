2f5964d9eb609fd0c115b1e9a534b38bd03422e2
Afeh <balogunafebu22@gmail.com>
2023-07-05 09:35:36 +0100

    Print first function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_message - function prints first before main
+ * Return: nothing
+ */
+
+
+void print_message()__attribute__((constructot));
+
+void print_message()
+{
+	printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+}
