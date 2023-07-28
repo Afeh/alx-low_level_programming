5bf0c93afcae6f5330a206e5ed81b9db06db924b
Afeh <balogunafebu22@gmail.com>
2023-07-29 00:17:39 +0100

    Betty corrections

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -245,8 +245,8 @@ void close_elf(int elf)
 
 
 /**
- * main - displays the information contained in the elf header 
- * 
+ * main - displays the information contained in the elf header
+ *
  * @argc: the number of arguments supplied to the program
  * @argv: An array of pointers to the arguments
  *
