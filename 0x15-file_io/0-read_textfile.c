4e73a805aeff25c7e48287551619985c4e569777
Afeh <balogunafebu22@gmail.com>
2023-07-28 20:49:56 +0100

    Betty correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * read_textfile - function that reads a text file and prints it
+ * to the POSIX stdout.
+ *
+ * @filename: name of the file
+ * @letters: number of the letters it should read and print
+ *
+ * Return: the number of letters it could read and print
+ */
+
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -9,21 +19,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_written;
 
 
-	if (filename == NULL)
+	buffer = (char *)malloc(letters * sizeof(char));
+
+	if (buffer == NULL || filename == NULL)
+	{
 		return (0);
+	}
 
 	file_descriptor = open(filename, O_RDONLY);
 
 	if (file_descriptor == -1)
 		return (0);
-
-	buffer = (char *)malloc(letters * sizeof(char));
-
-	if (buffer == NULL)
-	{
-		close(file_descriptor);
-		return (0);
-	}
 
 	bytes_read = read(file_descriptor, buffer, letters);
 
