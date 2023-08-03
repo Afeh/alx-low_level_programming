f4faaa7df9f8de300cecf5ecfc456d8f44380d6b
Afeh <balogunafebu22@gmail.com>
2023-08-03 16:32:47 +0100

    First and second tasks

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ *
+ * @n: The number to print in binary.
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask;
+	int leadingZeros;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
+	leadingZeros = 1;
+
+	while (mask != 0)
+	{
+		if (n & mask)
+		{
+			leadingZeros = 0;
+			_putchar('1');
+		}
+		else if (!leadingZeros)
+		{
+			_putchar('0');
+		}
+
+		mask >>= 1;
+	}
+}
